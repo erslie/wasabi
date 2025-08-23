2489797021939de6235a6e8252542fe8e8c374d2
erslie <kbyk0416@gmail.com>
2025-08-23 19:54:27 +0900

    p380

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 
 pub mod allocator;
 pub mod graphics;
+pub mod init;
 pub mod qemu;
 pub mod result;
 pub mod serial;
@@ -23,8 +24,6 @@ fn efi_main(
     image_handle: uefi::EfiHandle,
     efi_system_table: &uefi::EfiSystemTable,
 ) {
-    let mut memory_map = uefi::MemoryMapHolder::new();
-    uefi::exit_from_efi_boot_services(image_handle, efi_system_table, &mut memory_map);
-    allocator::ALLOCATOR.init_with_mmap(&memory_map);
+    init::init_basic_runtime(image_handle, efi_system_table);
     run_unit_tests()
 }
